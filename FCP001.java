0ce7f2560059b43302d202f0a165603ec544d646
Xcelevator-Vettrivel <Vettrivel.2524@xcelevate.org>
2025-07-23 06:34:25 +0530

    FCP001

---
+++ FCP001.java
@@ -0,0 +1,12 @@
+public class FCP001{
+public static void main(String[]args){
+
+int date = 1;
+int month = 9;
+int year = 2004;
+
+System.out.println(date);
+System.out.println(month);
+System.out.println(year);
+}
+}
