f4c05ce68a21144803cba77035a0c69dd84c1ba4
Xcelevator-Vettrivel <Vettrivel.2524@xcelevate.org>
2025-07-23 10:42:08 +0530

    FCP005

---
+++ FCP005.java
@@ -0,0 +1,13 @@
+public class FCP005{
+public static void main(String[]args){
+
+int a = 11;
+int b = 3;
+int c = a/b;
+int d = a%b;
+System.out.println(a);
+System.out.println(b);
+System.out.println("Quotient="+c);
+System.out.println("Remainder="+d);
+}
+}
