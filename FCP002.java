165f96b4d22cdd2c53a6153a56d2268b4fbc40b9
Xcelevator-Vettrivel <Vettrivel.2524@xcelevate.org>
2025-07-23 09:42:56 +0530

    FCP002

---
+++ FCP002.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class FCP002{
+public static void main(String[]args){
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the input:");
+int a = sc.nextInt();
+System.out.println(a);
+
+
+}
+}
