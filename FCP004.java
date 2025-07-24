39db3dd262f15eaa224b5d2f92e03b5759cd353f
Xcelevator-Vettrivel <Vettrivel.2524@xcelevate.org>
2025-07-24 12:27:08 +0530

    FCP004

---
+++ FCP004.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class FCP004{
+public static void main(String[]args){
+Scanner scan = new Scanner(System.in);
+System.out.println("Enter the Numbers :");
+int a = scan.nextInt();
+int b = scan.nextInt();
+
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+
+}
+}
