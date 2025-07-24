1bc2408926fa169234f5fcf76db8cc3c02b86e8f
Xcelevator-Vettrivel <Vettrivel.2524@xcelevate.org>
2025-07-24 12:29:03 +0530

    FCP006

---
+++ FCP006.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class FCP006{
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the numbers :");
+int a = sc.nextInt();
+int b = a % 10;
+System.out.println("The Last Digit is "+b);
+
+}
+}
